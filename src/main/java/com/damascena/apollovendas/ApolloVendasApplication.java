e2448dff5c8cb87c1bc91aacfed056d09d97c5f1
Lucas Damascena <damascena.lg@gmail.com>
2022-09-25 20:05:54 -0300

    Primeiro Commit

---
+++ src/main/java/com/damascena/apollovendas/ApolloVendasApplication.java
@@ -0,0 +1,13 @@
+package com.damascena.apollovendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApolloVendasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApolloVendasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/damascena/apollovendas/ApolloVendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.damascena.apollovendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApolloVendasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
