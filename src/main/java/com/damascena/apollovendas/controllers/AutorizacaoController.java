ccf045098d4a0310a6d53102a0d153c8af9b091e
Lucas Damascena <damascena.lg@gmail.com>
2022-10-20 13:41:13 -0300

    Refresh Token

---
+++ src/main/java/com/damascena/apollovendas/controllers/AutorizacaoController.java
@@ -0,0 +1,31 @@
+package com.damascena.apollovendas.controllers;
+
+import com.damascena.apollovendas.security.JWTUtil;
+import com.damascena.apollovendas.security.UsuarioSpringSecurity;
+import com.damascena.apollovendas.services.UsuarioService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletResponse;
+
+@RestController
+@RequestMapping(value = "/auth")
+public class AutorizacaoController {
+
+    private JWTUtil jwtUtil;
+
+    public AutorizacaoController(JWTUtil jwtUtil) {
+        this.jwtUtil = jwtUtil;
+    }
+
+    @PostMapping(value = "/refresh_token")
+    public ResponseEntity refreshToken(HttpServletResponse httpServletResponse) {
+        UsuarioSpringSecurity usuarioSpringSecurity = UsuarioService.autenticado();
+        String token = jwtUtil.gerarToken(usuarioSpringSecurity.getUsername());
+        httpServletResponse.addHeader("Authorization", "Bearer " + token);
+
+        return ResponseEntity.noContent().build();
+    }
+}
